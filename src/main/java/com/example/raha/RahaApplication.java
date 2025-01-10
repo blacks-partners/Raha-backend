8fcccf875b8b146be2f5c41b13947af6f0d3b12c
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2025-01-10 10:38:32 +0900

    git init

---
+++ src/main/java/com/example/raha/RahaApplication.java
@@ -0,0 +1,13 @@
+package com.example.raha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahaApplication.class, args);
+	}
+
+}
