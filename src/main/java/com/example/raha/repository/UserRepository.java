545e0215eae7ff2a0dace4f76d4cf6ebf1f700ff
hosodatomoya <tomoya.hosoda@rakus-partners.co.jp>
2025-01-14 15:05:07 +0900

    UserServiceメソッド追加

--- src/main/java/com/example/raha/repository/UserRepository.java
+++ src/main/java/com/example/raha/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.raha.repository;
 
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
--- src/main/java/com/example/raha/service/UserService.java
+++ src/main/java/com/example/raha/service/UserService.java
@@ -1,5 +1,8 @@
 package com.example.raha.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.raha.domain.User;
@@ -9,13 +12,25 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository repository;
 
     public User load(Integer id) {
         return repository.load(id);
 
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.loadByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("ユーザーが見つかりません");
+        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
     }
 
 }
