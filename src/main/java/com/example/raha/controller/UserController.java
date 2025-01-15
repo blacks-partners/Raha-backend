062cf02d5b8b67f0267246e75e0802c5ce4f08f3
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2025-01-15 17:13:08 +0900

    いらないところ削除

--- src/main/java/com/example/raha/controller/UserController.java
+++ src/main/java/com/example/raha/controller/UserController.java
@@ -19,19 +19,4 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-
-    private final UserService service;
-
-    /**
-     * ユーザー情報詳細の取得
-     * 
-     * @return user ユーザー情報
-     */
-    @GetMapping("/{userId}")
-    public User userDetails(@PathVariable Integer userId) {
-        User user = service.load(userId);
-        return user;
-
-    }
-
 }
