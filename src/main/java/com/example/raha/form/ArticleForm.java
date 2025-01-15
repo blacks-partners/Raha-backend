ea631605cd9838377e61acb13225bd1f7abef651
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2025-01-15 16:28:51 +0900

    新規投稿機能作成完了

---
+++ src/main/java/com/example/raha/form/ArticleForm.java
@@ -0,0 +1,18 @@
+package com.example.raha.form;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArticleForm {
+
+    private String title;
+    private String content;
+    private Integer userId;
+
+}
