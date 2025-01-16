31bfc9ea641ed20cadd0f300f5143436faf1f086
nakaryunosuke <naka.sava0309@gmail.com>
2025-01-16 10:39:03 +0900

    Add: importの追加。

--- src/main/java/com/example/raha/repository/UserRepository.java
+++ src/main/java/com/example/raha/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.raha.repository;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
