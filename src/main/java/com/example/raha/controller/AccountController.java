2eb34b4cc8acad6178dbf8734a009c884bd28128
nakaryunosuke <naka.sava0309@gmail.com>
2025-01-16 14:27:37 +0900

    Add: ユーザー登録時にlocationヘッダーが返るように修正。

--- src/main/java/com/example/raha/controller/AccountController.java
+++ src/main/java/com/example/raha/controller/AccountController.java
@@ -1,15 +1,18 @@
 package com.example.raha.controller;
 
+import java.net.URI;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.raha.domain.User;
 import com.example.raha.error.ConflictException;
-import com.example.raha.form.UserForm;
+import com.example.raha.form.RegisterUserForm;
 import com.example.raha.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -26,17 +29,23 @@ public class AccountController {
 
     /**
      * ユーザー登録をする。
-     * @param UserForm
+     * @param RegisterUserForm
      * @return ユーザーIDを返す。
      */
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public Integer register(@RequestBody UserForm form){
+    public ResponseEntity<Void> register(@RequestBody RegisterUserForm form){
         User registeredUser = service.findByEmail(form.getEmail());
         if(registeredUser != null){
             throw new ConflictException("入力されたメールアドレスは既に登録されています");
         }
         Integer id = service.register(form);
-        return id;
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
