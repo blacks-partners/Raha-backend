44a7fd665aad5e2b1d74376a93b7c7755719f3f8
hosodatomoya <tomoya.hosoda@rakus-partners.co.jp>
2025-01-17 13:00:22 +0900

    トークン取得処理の切り分け

--- src/main/java/com/example/raha/controller/AccountController.java
+++ src/main/java/com/example/raha/controller/AccountController.java
@@ -1,9 +1,7 @@
 package com.example.raha.controller;
 
-import java.time.OffsetDateTime;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.example.raha.domain.User;
 import com.example.raha.error.invalidAuthenticationException;
 import com.example.raha.form.LoginForm;
@@ -34,8 +30,6 @@ public class AccountController {
     private final UserService service;
     private final DaoAuthenticationProvider provider;
 
-    @Value("${jwt.secret}")
-    private String secret;
 
     /**
      * ログイン
@@ -51,11 +45,8 @@ public class AccountController {
             provider.authenticate(new UsernamePasswordAuthenticationToken(form.getEmail(), form.getPassword()));
             User user = service.loadByEmail(form.getEmail());
 
-            // userIdクレーム、60分の有効期限を持つJWTトークンの生成
-            String token = JWT.create().withClaim("userId", user.getUserId())
-                    .withExpiresAt(OffsetDateTime.now().plusDays(1).toInstant()).sign(Algorithm.HMAC256(secret));
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("X-AUTH-TOKEN", "Bearer " + token);
+            // JWTトークンの生成
+            HttpHeaders headers = service.createJwtHeader(user.getUserId());
             Map<String, Integer> response = Map.of("userId", user.getUserId());
             return new ResponseEntity<>(response, headers, HttpStatus.OK);
 
