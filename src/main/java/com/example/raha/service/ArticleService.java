87bccc61d2dbe24f046c2ab2ac52a4497555b015
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2025-01-20 10:35:42 +0900

    修正完了

--- src/main/java/com/example/raha/service/ArticleService.java
+++ src/main/java/com/example/raha/service/ArticleService.java
@@ -2,8 +2,6 @@ package com.example.raha.service;
 
 import java.util.List;
 
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.raha.domain.Article;
