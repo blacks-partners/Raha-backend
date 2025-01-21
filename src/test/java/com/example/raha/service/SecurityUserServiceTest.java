d9bbeed51a3a8d59b2c8fd2bd3877ba3848d9ae2
hosodatomoya <tomoya.hosoda@rakus-partners.co.jp>
2025-01-21 10:55:23 +0900

    serviceテスト土台作成

---
+++ src/test/java/com/example/raha/service/SecurityUserServiceTest.java
@@ -0,0 +1,41 @@
+package com.example.raha.service;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.raha.domain.User;
+import com.example.raha.repository.UserRepository;
+
+@SpringBootTest
+public class SecurityUserServiceTest {
+    
+    @InjectMocks
+    private SecurityUserService securityUserService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void testLoadUserByUsername() {
+        User user = new User();
+        user.setUserId(1);
+        user.setName("test");
+        user.setEmail("test@example.com");
+        user.setPassword("password");
+        user.setIntroduction("test introduction");
+        user.setCreatedAt(null);
+        user.setUpdatedAt(null);
+
+        String email = "test@example.com";
+
+        when(userRepository.loadByEmail(email)).thenReturn(user);
+
+        UserDetails userDetails = securityUserService.loadUserByUsername(email);
+        
+    }
+}
