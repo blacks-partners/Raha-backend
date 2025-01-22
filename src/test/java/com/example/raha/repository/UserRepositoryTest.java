e7706f0b71c5200dcfb1260e32fa2b3ff8cd846c
nakaryunosuke <naka.sava0309@gmail.com>
2025-01-22 10:16:45 +0900

    Fix: @Sqlアノテーションでsqlを呼び出すやり方に変更。

--- src/test/java/com/example/raha/repository/UserRepositoryTest.java
+++ src/test/java/com/example/raha/repository/UserRepositoryTest.java
@@ -1,40 +1,26 @@
 package com.example.raha.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import javax.sql.DataSource;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.raha.domain.User;
 
 @SpringBootTest
+@Sql("/testData.sql")
 @Transactional
 public class UserRepositoryTest {
     
     @Autowired
     private UserRepository repository;
 
-    @BeforeAll
-    static void testBeforeAll(@Autowired DataSource detaSource){
-        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScript(
-            new ClassPathResource("testData.sql"));
-        populator.execute(detaSource);
-    }
-
     @Test
     void testDelete() {
         // repository.delete(1);
-        // User result = repository.load(1);
-        // assertNull(result);
-
+        // verify(this.repository, times(1)).delete(1);
     }
 
     @Test
@@ -64,13 +50,5 @@ public class UserRepositoryTest {
     void testUpdate() {
         
 
-    }
-
-    @AfterAll
-    static void testAfterAll(@Autowired DataSource dataSource) {
-        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScripts(
-        new ClassPathResource("/testData.sql"));
-        populator.execute(dataSource);
     }
 }
