98201e6b2feb89be1ac55166a8e1f7d30c765a46
nakaryunosuke <naka.sava0309@gmail.com>
2025-01-22 13:05:01 +0900

    Add: DisplayNameの追加。

--- src/test/java/com/example/raha/repository/UserRepositoryTest.java
+++ src/test/java/com/example/raha/repository/UserRepositoryTest.java
@@ -2,6 +2,8 @@ package com.example.raha.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +14,11 @@ import com.example.raha.domain.User;
 import com.example.raha.form.RegisterUserForm;
 import com.example.raha.form.UpdateUserForm;
 
+/**
+ * UserRepositoryのテスト。
+ * @author R.naka
+ */
+
 @SpringBootTest
 @Sql("/testData.sql")
 @Transactional
@@ -21,6 +28,7 @@ public class UserRepositoryTest {
     private UserRepository repository;
 
     @Test
+    @DisplayName("User情報が削除できているか確認するテスト。")
     void testDelete() {
         Integer userId = 1;
         repository.delete(userId);
@@ -29,6 +37,7 @@ public class UserRepositoryTest {
     }
 
     @Test
+    @DisplayName("emailからUser（パスワード）が取り出せているかのテスト。")
     void testFindByEmail() {
         String email = "demo_user@example.com";
         Integer id = 1;
@@ -42,6 +51,7 @@ public class UserRepositoryTest {
     }
 
     @Test
+    @DisplayName("UserFormの情報から登録処理ができているかのテスト。")
     void testInsert() {
         RegisterUserForm form = new RegisterUserForm("taro", "taro@taro", "taro");
         Integer id = 2;
@@ -51,6 +61,7 @@ public class UserRepositoryTest {
     }
 
     @Test
+    @DisplayName("idのUserが呼び出せているかのテスト。")
     void testLoad() {
         User result = repository.load(1);
         if(result==null){
@@ -61,6 +72,7 @@ public class UserRepositoryTest {
     }
 
     @Test
+    @DisplayName("emailからUser（パスワードあり）が取得できているかのテスト。")
     void testLoadByEmail() {
         User result = repository.findByEmail("demo_user@example.com");
         User user = repository.load(1);
@@ -68,6 +80,7 @@ public class UserRepositoryTest {
     }
 
     @Test
+    @DisplayName("User情報の更新処理が実行できているかのテスト。")
     void testUpdate() {
         UpdateUserForm form = new UpdateUserForm("taro", "taro@taro", "taroです。");
         Integer id = 1;
