d9467e7ba9ecf8b94ea70dd0876c28590f1009a6
nakaryunosuke <naka.sava0309@gmail.com>
2025-01-23 10:38:11 +0900

    Add:UserControllerTestの一時保存。

--- src/test/java/com/example/raha/controller/UserControllerTest.java
+++ src/test/java/com/example/raha/controller/UserControllerTest.java
@@ -1,34 +1,52 @@
 package com.example.raha.controller;
 
-import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.raha.domain.User;
 import com.example.raha.service.UserService;
 
+/**
+ * UserControllerクラスのテスト。
+ * 
+ * @author R.Naka
+ */
 @SpringBootTest
-@Sql("/TestSql.sql")
+@AutoConfigureMockMvc(addFilters = false)
 @Transactional
 public class UserControllerTest {
-    @InjectMocks
-    private UserController controller;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Mock
+    @MockitoBean
     private UserService service;
 
     @Test
-    void testDelete() {
+    @DisplayName("Userの退会処理を確認するテスト。")
+    void testDelete() throws Exception{
         User user = new User(1, "taro", "taro@taro", "password", "hello", LocalDateTime.now(), LocalDateTime.now());
-        Integer id = 1;
-        doNothing().when(service).delete(id);
+        Integer userId = user.getUserId();
+        Mockito.when(service.load(userId)).thenReturn(user);
+
+        mockMvc.perform(
+                delete("/users/{userId}", 1)
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
     }
 
     @Test
@@ -37,7 +55,20 @@ public class UserControllerTest {
     }
 
     @Test
-    void testUserDetails() {
+    @DisplayName("User詳細処理を確認するテスト。")
+    void testUserDetails() throws Exception {
+        User user = new User(1, "taro", "taro@taro", "password", "hello", LocalDateTime.now(), LocalDateTime.now());
+        Integer userId = user.getUserId();
+        Mockito.when(service.load(userId)).thenReturn(user);
+
+        mockMvc.perform(
+                get("/users/{userId}", 1)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(7))
+                .andExpect(jsonPath("$.name").value("taro"))
+
+                ;
 
     }
 }
