f8b314291aa4138f8771342b57107a4410fc7220
nakaryunosuke <naka.sava0309@gmail.com>
2025-01-27 09:53:10 +0900

    Fix: UserServiceクラスのfindByEmailメソッドの不要なif文の削除。

--- src/main/java/com/example/raha/service/UserService.java
+++ src/main/java/com/example/raha/service/UserService.java
@@ -62,9 +62,6 @@ public class UserService {
      * @return user ユーザー
      */
     public User findByEmail(String email){
-        if(email == null){
-            return null;
-        }
         User user = repository.findByEmail(email);
         return user;
     }
