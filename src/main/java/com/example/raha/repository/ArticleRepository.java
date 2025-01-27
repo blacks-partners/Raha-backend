eba08f371e26cd01c07f70dcb535d3a998ddd350
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2025-01-27 11:45:27 +0900

    ROW_MAPPERの修正

--- src/main/java/com/example/raha/repository/ArticleRepository.java
+++ src/main/java/com/example/raha/repository/ArticleRepository.java
@@ -30,6 +30,23 @@ public class ArticleRepository {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
+    private static final RowMapper<Article> ARTICLE_ROWMAPPER = (rs, i) -> {
+        Article article = new Article();
+        article.setArticleId(rs.getInt("a_id"));
+        article.setTitle(rs.getString("a_title"));
+        article.setContent(rs.getString("a_content"));
+        article.setCreatedAt(rs.getTimestamp("a_created_at").toLocalDateTime());
+        article.setUpdatedAt(rs.getTimestamp("a_updated_at").toLocalDateTime());
+        article.setCommentList(null);
+
+        User user = new User();
+        user.setUserId(rs.getInt("u_id"));
+        user.setName(rs.getString("u_name"));
+
+        article.setUser(user);
+        return article;
+    };
+
     private static final RowMapper<Article> USER_ARTICLE_ROWMAPPER = (rs, i) -> {
         Article article = new Article();
         article.setArticleId(rs.getInt("a_id"));
@@ -99,7 +116,7 @@ public class ArticleRepository {
 
         String sql = "SELECT a.id as a_id,a.title as a_title,a.content as a_content,a.created_at as a_created_at,a.updated_at as a_updated_at,u.id as u_id, u.name as u_name FROM articles as a LEFT OUTER JOIN users as u on a.user_id = u.id ORDER BY a.created_at DESC,a.id DESC";
 
-        List<Article> articleList = jdbcTemplate.query(sql, USER_ARTICLE_ROWMAPPER);
+        List<Article> articleList = jdbcTemplate.query(sql, ARTICLE_ROWMAPPER);
 
         return articleList;
     }
