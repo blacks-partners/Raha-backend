791c20eff753b310b5ac5980a1a8e64619cc4935
nakaryunosuke <naka.sava0309@gmail.com>
2025-01-27 11:07:10 +0900

    Fix: importの削除。

--- src/test/java/com/example/raha/controller/UserControllerTest.java
+++ src/test/java/com/example/raha/controller/UserControllerTest.java
@@ -1,7 +1,6 @@
 package com.example.raha.controller;
 
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
