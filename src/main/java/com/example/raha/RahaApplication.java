faf2a6f0311241b2f83404ccbc55ad98b0645caa
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2025-01-28 10:33:10 +0900

    コメント降順を昇順に変更する

--- src/main/java/com/example/raha/RahaApplication.java
+++ src/main/java/com/example/raha/RahaApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class RahaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RahaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RahaApplication.class, args);
+    }
 
 }
